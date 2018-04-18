6dfb3eaf0a11f4c700ade56b0c309e26b6fa26cc
Vladislav Sumin <vladislavsumin@mail.ru>
2018-04-19 00:18:49 +0300

    initialize spring project with    spring-web    spring-security    lombok    freemarker    mysql

---
+++ src/main/java/ru/falseteam/vktests/VktestsApplication.java
@@ -0,0 +1,12 @@
+package ru.falseteam.vktests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VktestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VktestsApplication.class, args);
+    }
+}
