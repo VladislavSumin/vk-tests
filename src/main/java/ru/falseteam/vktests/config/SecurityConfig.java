6d3248cc2ca723b91edb6535b765f80eb04d63c4
Vladislav Sumin <vladislavsumin@mail.ru>
2018-04-19 13:31:18 +0300

    configure spring security and BCrypt password encryprion

--- src/main/java/ru/falseteam/vktests/config/SecurityConfig.java
+++ src/main/java/ru/falseteam/vktests/config/SecurityConfig.java
@@ -1,6 +1,39 @@
 package ru.falseteam.vktests.config;
 
-//@Configuration
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder getBCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(getBCryptPasswordEncoder())
+                .withUser("user").password(getBCryptPasswordEncoder().encode("123")).roles("USER");
+    }
 }
